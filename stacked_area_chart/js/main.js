80e2efc019c0578dda0d8e9d8905267f7a18d6fb
Jorge Longoria <jlongoria@ptsteams.com>
2017-03-30 10:42:27 -0500

    fixed bug: chart area not aligned

--- stacked_area_chart/js/main.js
+++ stacked_area_chart/js/main.js
@@ -2,7 +2,7 @@ let margin = {
   top:    20,
   right:  20,
   bottom: 30,
-  left:   40
+  left:   50
 };
 
 let exteriorWidth  = 550,
@@ -22,27 +22,27 @@ let svg =
 var parseDate = d3.timeParse("%Y %b %d");
 
 //var x = d3.scaleTime().range([0, interiorWidth]),
-var x = d3.scaleBand().range([0, interiorWidth]);
+var x = d3.scaleBand().range([0, interiorWidth]).paddingInner(1);
     y = d3.scaleLinear().range([interiorHeight, 0]),
     z = d3.scaleOrdinal(d3.schemeCategory10);
 
 var stack = d3.stack();
 
 var area = d3.area()
-    .x(function(d, i) { return x(d.data.category); })
+    .x( function(d) { return x(d.data.category); })
     .y0(function(d) { return y( d[0] ); })
     .y1(function(d) { return y( d[1] ); });
 
 let data = {
   raw: [
-    {category: "A", series: "first",   measure: .25},
-    {category: "B", series: "first",   measure: .35},
-    {category: "C", series: "first",   measure: .45},
-    {category: "D", series: "first",   measure: .55},
-    {category: "A", series: "second",  measure: .17},
-    {category: "B", series: "second",  measure: .27},
-    {category: "C", series: "second",  measure: .37},
-    {category: "D", series: "second",  measure: .47}
+    {category: "A", series: "first",   measure: 25},
+    {category: "B", series: "first",   measure: 35},
+    {category: "C", series: "first",   measure: 15},
+    {category: "D", series: "first",   measure: 25},
+    {category: "A", series: "second",  measure: 17},
+    {category: "B", series: "second",  measure: 47},
+    {category: "C", series: "second",  measure: 37},
+    {category: "D", series: "second",  measure: 43}
   ],
   munged: []
 };
@@ -66,15 +66,24 @@ data.munged = rollup.map(function(d) {
 var keys = data.raw.map(function(d) { return d.series; })
                    .filter(function(d,i,a) { return a.indexOf(d) == i; });
 
+stack.keys(keys);
 
 //x.domain(d3.extent(data, function(d) { return d.date; }));
 x.domain(
   data.munged.map(function(d) { return d.category; })
 );
 
+let arr = [];
+stack(data.munged).forEach(function(series) {
+  series.forEach(function(category) {
+    arr = arr.concat(category);
+  });
+});
+
+y.domain([0, d3.max(arr)]).nice();
+
 z.domain(keys);
 
-stack.keys(keys);
 
 var layer =
   svg.selectAll(".layer")
@@ -86,8 +95,8 @@ var layer =
 layer.append("path")
     .attr("class", "area")
     .style("fill", function(d) { return z(d.key); })
-    .attr("d", area)
-    .append("title").text(function(d) { return d; });
+    .attr("d", area);
+    //.append("title").text(function(d) { return d; });
     //tooltip doesn't work
 
 layer.filter(function(d) { return d[d.length - 1][1] - d[d.length - 1][0] > 0.01; })
@@ -95,7 +104,7 @@ layer.filter(function(d) { return d[d.length - 1][1] - d[d.length - 1][0] > 0.01
     .attr("x", interiorWidth - 6)
     .attr("y", function(d) { return y((d[d.length - 1][0] + d[d.length - 1][1]) / 2); })
     .attr("dy", ".35em")
-    .style("font", "10px sans-serif")
+    .style("font", "15px Helvetica")
     .style("text-anchor", "end")
     .text(function(d) { return d.key; });
 
@@ -106,7 +115,8 @@ svg.append("g")
 
 svg.append("g")
     .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y).ticks(10, "%"));
+    .call(d3.axisLeft(y).ticks(10));
+    //.call(d3.axisLeft(y).ticks(10, "%"));
 
 
 function translate(x, y=0) {
