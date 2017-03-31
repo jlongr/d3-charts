6f9c2ddbd46fc56a7c4d7533d2749ad7c8dbe942
Jorge Longoria <jlongoria@ptsteams.com>
2017-03-31 12:38:40 -0500

    feature: series grouping

--- line_chart/js/main.js
+++ line_chart/js/main.js
@@ -19,48 +19,86 @@ let svg =
     .append("g")
       .attr("transform", translate(margin.left, margin.top));
 
-var parseTime = d3.timeParse("%d-%b-%y");
+let parseTime = d3.timeParse("%d-%b-%y");
 
-/*var x = d3.scaleTime()
+/*let x = d3.scaleTime()
     .rangeRound([0, interiorWidth]);*/
-var x = d3.scaleBand()
+let x = d3.scaleBand()
           .rangeRound([0, interiorWidth]);
 
-var y = d3.scaleLinear()
+let y = d3.scaleLinear()
     .rangeRound([interiorHeight, 0]);
 
-var line = d3.line()
+let z = d3.scaleOrdinal(d3.schemeCategory10);
+
+let line = d3.line()
+    .curve(d3.curveBasis)
     .x(function(d) { return x(d.category); })
     .y(function(d) { return y(d.measure); });
 
 let data = [
-  {category: "A", measure: .08167},
-  {category: "B", measure: .01492},
-  {category: "C", measure: .02782},
-  {category: "D", measure: .04253},
-  {category: "E", measure: .12702},
-  {category: "F", measure: .02288},
-  {category: "G", measure: .02015},
-  {category: "H", measure: .06094},
-  {category: "I", measure: .06966},
-  {category: "J", measure: .00153},
-  {category: "K", measure: .00772},
-  {category: "L", measure: .04025},
-  {category: "M", measure: .02406},
-  {category: "N", measure: .06749},
-  {category: "O", measure: .07507},
-  {category: "P", measure: .01929},
-  {category: "Q", measure: .00095},
-  {category: "R", measure: .05987},
-  {category: "S", measure: .06327},
-  {category: "T", measure: .09056},
-  {category: "U", measure: .02758},
-  {category: "V", measure: .00978},
-  {category: "W", measure: .02360},
-  {category: "X", measure: .00150},
-  {category: "Y", measure: .01974},
-  {category: "Z", measure: .00074}
+  {category: "A", series: "first", measure: .08167},
+  {category: "B", series: "first", measure: .01492},
+  {category: "C", series: "first", measure: .02782},
+  {category: "D", series: "first", measure: .04253},
+  {category: "E", series: "first", measure: .12702},
+  {category: "F", series: "first", measure: .02288},
+  {category: "G", series: "first", measure: .02015},
+  {category: "H", series: "first", measure: .06094},
+  {category: "I", series: "first", measure: .06966},
+  {category: "J", series: "first", measure: .00153},
+  {category: "K", series: "first", measure: .00772},
+  {category: "L", series: "first", measure: .04025},
+  {category: "M", series: "first", measure: .02406},
+  {category: "N", series: "first", measure: .06749},
+  {category: "O", series: "first", measure: .07507},
+  {category: "P", series: "first", measure: .01929},
+  {category: "Q", series: "first", measure: .00095},
+  {category: "R", series: "first", measure: .05987},
+  {category: "S", series: "first", measure: .06327},
+  {category: "T", series: "first", measure: .09056},
+  {category: "U", series: "first", measure: .02758},
+  {category: "V", series: "first", measure: .00978},
+  {category: "W", series: "first", measure: .02360},
+  {category: "X", series: "first", measure: .00150},
+  {category: "Y", series: "first", measure: .01974},
+  {category: "Z", series: "first", measure: .00074},
+
+  {category: "A", series: "second", measure: .8167},
+  {category: "B", series: "second", measure: .1492},
+  {category: "C", series: "second", measure: .2782},
+  {category: "D", series: "second", measure: .4253},
+  {category: "E", series: "second", measure: .2702},
+  {category: "F", series: "second", measure: .2288},
+  {category: "G", series: "second", measure: .2015},
+  {category: "H", series: "second", measure: .6094},
+  {category: "I", series: "second", measure: .6966},
+  {category: "J", series: "second", measure: .0153},
+  {category: "K", series: "second", measure: .0772},
+  {category: "L", series: "second", measure: .4025},
+  {category: "M", series: "second", measure: .2406},
+  {category: "N", series: "second", measure: .6749},
+  {category: "O", series: "second", measure: .7507},
+  {category: "P", series: "second", measure: .1929},
+  {category: "Q", series: "second", measure: .0095},
+  {category: "R", series: "second", measure: .5987},
+  {category: "S", series: "second", measure: .6327},
+  {category: "T", series: "second", measure: .9056},
+  {category: "U", series: "second", measure: .2758},
+  {category: "V", series: "second", measure: .0978},
+  {category: "W", series: "second", measure: .2360},
+  {category: "X", series: "second", measure: .0150},
+  {category: "Y", series: "second", measure: .1974},
+  {category: "Z", series: "second", measure: .0074}
 ];
+
+let series = ["first", "second"].map(function(datum) {
+    return {
+      id: datum,
+      values: data.filter(function(d) { return d.series == datum; })
+        .map(function(d) { return {category: d.category, measure: d.measure}; })
+    };
+});
 
 x.domain(
   data.map(function(d) { return d.category; })
@@ -70,6 +108,8 @@ x.domain(
 y.domain(
   d3.extent(data, function(d) { return d.measure; })
 );
+
+z.domain(series.map(function(c) { return c.id; }));
 
 svg.append("g")
     .attr("transform", translate(0, interiorHeight))
@@ -87,16 +127,21 @@ svg.append("g")
     .attr("text-anchor", "end")
     .text("Axis Title");
 
-svg.append("path")
-   .datum(data)
-     .attr("fill", "none")
-     .attr("stroke", "steelblue")
-     .attr("stroke-linejoin", "round")
-     .attr("stroke-linecap", "round")
-     .attr("stroke-width", 1.5)
-     .attr("d", line)
-  .append("title")
-     .text(function(d) { return d.category +": "+ d.measure; });
+let path =
+  svg.selectAll(".lines")
+    .data(series)
+    .enter()
+    .append("g")
+      .attr("class", "lines");
+
+path.append("path")
+        .attr("class", "line")
+        .attr("d", function(d) { return line(d.values); })
+        .attr("fill", "none")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("stroke", function(d) { return z(d.id); });
 
 
 function translate(x, y=0) {
