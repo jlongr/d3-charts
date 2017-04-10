60f4f951ad8e823ab244c2c37cc8e3bf21ceafd5
Jorge Longoria <jlongoria@ptsteams.com>
2017-04-10 15:40:18 -0500

    bar chart: using data and colors modules

--- bar_chart/js/main.js
+++ bar_chart/js/main.js
@@ -28,57 +28,27 @@ let svg =
 
 let x =
   d3.scaleBand()
-    .rangeRound([0, interiorWidth])
+    .domain( data.categoryNames() )
+    .rangeRound( [0, interiorWidth] )
     .paddingInner(0.1);
 
 let x1 =
   d3.scaleBand()
+    .domain( data.seriesNames() )
+    .rangeRound( [0, x.bandwidth()] )
     .padding(0.05);
 
 let y =
   d3.scaleLinear()
-    .rangeRound([interiorHeight, 0]);
+    .domain( data.extent() ).nice()
+    .rangeRound( [interiorHeight, 0] );
 
 
-var z =
-  d3.scaleOrdinal()
-    .range(colorPalette.reverse());
+var color = colors.generateScale({
+  scale: "ordinal",
+  palette: "interpolateViridis"
+});
 
-let data = [
-  {category: "A", series: "first",  measure: 67},
-  {category: "B", series: "first",  measure: 92},
-  {category: "C", series: "first",  measure: 82},
-  {category: "D", series: "first",  measure: 53},
-
-  {category: "A", series: "second", measure: 99},
-  {category: "B", series: "second", measure: 88},
-  {category: "C", series: "second", measure: 15},
-  {category: "D", series: "second", measure: 94},
-
-  {category: "A", series: "third", measure: 99},
-  {category: "B", series: "third", measure: 88},
-  {category: "C", series: "third", measure: 15},
-  {category: "D", series: "third", measure: 94},
-
-  {category: "A", series: "fourth", measure: 79},
-  {category: "B", series: "fourth", measure: 68},
-  {category: "C", series: "fourth", measure: 25},
-  {category: "D", series: "fourth", measure: 44}
-];
-
-x.domain(
-  data.map(function(d) { return d.category; })
-);
-
-x1.domain(
-    data.map(function(d) { return d.series; }) //unique keys
-        .filter(function(d,i,a) { return a.indexOf(d) == i; })
-  )
-  .rangeRound([0, x.bandwidth()]);
-
-y.domain(
-  [0, d3.max(data, function(d) { return d.measure; })]
-);
 
 svg.append("g")
     .attr("class", "axis axis--x")
@@ -87,7 +57,7 @@ svg.append("g")
 
 svg.append("g")
      .attr("class", "axis axis--y")
-   .call(d3.axisLeft(y).ticks(10))
+   .call(d3.axisLeft(y).ticks( data.seriesNames().length + 1 ))
    .append("text")
      .attr("transform", "rotate(-90)")
      .attr("y", 6)
@@ -99,20 +69,22 @@ svg.append("g")
      .style("font-family", "sans-serif");
 
 svg.selectAll(".bar")
-     .data(data)
+     .data( data.rows() )
    .enter().append("g")
-     .attr("transform", function(d) { return translate(x(d.category), 0); })
+     .attr("transform", function(d) {
+       return translate(x(d.category), 0);
+     })
    .selectAll("rect")
      .data(function(d) {
-             return [{key: d.series, value: d.measure }];
-       })
+        return [{key: d.series, value: d.measure }];
+      })
    .enter().append("rect")
      .attr("class", "bar")
      .attr("x", function(d) { return x1(d.key); })
      .attr("y", function(d) { return y(d.value); })
      .attr("width", x1.bandwidth())
      .attr("height", function(d) { return interiorHeight - y(d.value); })
-     .attr("fill", function(d) { return z(d.key); })
+     .attr("fill", function(d) { return color(d.key); })
    .append("title")
      .text(function(d) { return d.key +":  "+ d.value; });
 
