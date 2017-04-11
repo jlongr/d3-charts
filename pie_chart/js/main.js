82551e310c41382b22d6a5c915b6ba74b520b907
Jorge Longoria <jorge.r.longoria@gmail.com>
2017-04-10 21:34:09 -0500

    remove testing data

--- pie_chart/js/main.js
+++ pie_chart/js/main.js
@@ -2,18 +2,6 @@ let width  = 500,
     height = 300,
     radius = Math.min(width, height) / 2;
 
-/*
-let _data = [
-  {age: "<5",    population: 2704659},
-  {age: "5-13",  population: 4499890},
-  {age: "14-17", population: 2159981},
-  {age: "18-24", population: 3853788},
-  {age: "25-44", population: 14106543},
-  {age: "45-64", population: 8819342},
-  {age: "≥65",   population: 612463}
-];
-*/
-
 let color = colors.generateScale({
     scale: "ordinal",
     palette: "interpolateViridis",
